33eaf0893253e80c89601837d5646f4bc41467f4
aersosi <aersosi@avodaq.com>
2024-05-15 18:52:50 +0200

    [init] asciiArt

--- data/faceAsciiArt.js
+++ data/faceAsciiArt.js
@@ -1,15 +1,5 @@
-// export const faceAsciiArt = `
-// |￣￣￣￣￣￣￣￣￣￣￣|
-//     This is cute
-//       but not
-//      accessible
-// |＿＿＿＿＿＿＿＿＿＿＿|
-//    (\\__/)  ||
-//    (•ㅅ•)  ||
-//    /  　  づ`;
-
 // import chalk from 'chalk';
-//
+
 // export const faceAsciiArt = `
 // |￣￣￣￣￣￣￣￣￣￣|
 // ${chalk.hex('#ff7124').bold('     This is cute')}
@@ -21,29 +11,10 @@
 // ${chalk.hex('#ffffff')('  /  　  づ')}
 // `;
 
-// 20 zeilen hoch
-
 
 export const faceAsciiArt = `      
-  A::..      .  .  ...:..:.::I:IHIHIHHHHA.  .  . ..::I:. .
-  A::..      .  .  ...:..:.::I:IHIHIHHHHA.  .  . ..::I:. .
- :HI:.. .       . .. .:.:.::I:IHIHIIHIHHHA. .   .. .::I:. ..
- AI:.. .. .    . .. .:.:.::II:IHIIIHIHIHHHA.  .  . ..::I:. ..
-:HI:.. . .   .  . .. .::.:I:IHIHIIIHIHIIHHHA..  . .. .::I:. ..
-AI:.:.. .  .  .  ... .::.::I:IHIIHIHIHIHIHIHHA. .  . ..::I:. .
-HI:. .. . .  .  . .. .:..::IIHIHIHIIIIVHIIHHHVA.  . . .:::I:. . .
-HI:.. . .  .   . .. ..:.::I:IIHHIIHIHIHIHHHHV'  '.. . ..:::II: . .
-HI::.. .  .   .  .. .:..:::IIHIHIIVIVIIVHVV' .    .. . ..::III: .  .
-HI::... . . .  . ... ..:.:::IIHIVIVIVHVHVV. .  .   . .. .:.:III. .   .
-II::.:.. . .  .  .. ......:..IHVHIVVHVHV'.. . . . . '... .:.:IHI:..    .
-II:I::.. .  .   .  . .....::.:IHVHVVVHV:.. .  .  . .  .:..:::IIHII..
-:II:.:.:.. .  .   . ......:.:.:IVVHVVV:.:.. .  .  .  . :...:.:IHHI:..
- HI::.:. . . .  .  . ...:.::.::.VVHVV::.:.:.. .  . .. . :.. ..:IHHI::.''
- HII::.:.. .  .  . .. .:..:.'.  'VVVI::.::.:.. . .  . .. ':...:II:IIII::
- III::.:... .  .  . ...:.:... .   VII:I::.:.. .  .  .. . . :.:::...::.::
-  VII::.:.. . . . .. ...:....      VHI:I::.:.. .  . ... .. .::.:..:.:..:
-   VII::.:.. . .    ..:.::.. .     :HHII:I::.:.. . . .. ..  .'::':......
-   III:I::.. .. . . .. .:.:.. .    :VHIHI:I::.:... . . .. .. .':. .. .AH
-    III:I::.. . . .  .. ..:.. . .  ::HHIHII:I::.:... .. .. ... .:.::AHHH
-    ░▒▓█
+   |\\      _,,,---,,_
+   /,\`.-'\`'    -.  ;-;;,_
+  |,4-  ) )-,_..;\\ (  \`'-'
+ '---''(_/--'  \`-'\\_)  fL
 `
