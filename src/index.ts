402e8a2f4465ffacac630f48eb15511fa113f013
aersosi <aersosi@avodaq.com>
2024-06-18 12:46:19 +0200

    [fix] types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
 import { Core } from './core/functions.core.js';
+// import { Core } from './___core_old_ts/functions.core.js';
+// import { Core } from './___core/functions.core.js';
 
 new Core().showMenuIndex();
